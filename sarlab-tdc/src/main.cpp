5721a38b34b315c2d45c82e5c65d6ad69caa0883
Wyatt Gronnemose <wgronnemose@airadar.com>
2024-07-21 19:51:48 -0700

    Stub file for sarlab-tdc subproject

---
+++ sarlab-tdc/src/main.cpp
@@ -0,0 +1,11 @@
+/* Standard library headers */
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "==============================\n";
+    std::cout << "   SARLAB-TDC SAR PROCESSOR \n";
+    std::cout << "==============================\n";
+
+    return 0;
+}
