fc02c46294b2cff2629003f2499c873daba14d2b
Wyatt Gronnemose <wgronnemose@airadar.com>
2024-07-22 11:13:46 -0700

    Trim strings when getting argument values

--- sarlab-tdc/include/argparser.h
+++ sarlab-tdc/include/argparser.h
@@ -1,15 +1,16 @@
 /* Standard library headers */
+#include <string>
 #include <string_view>
 #include <vector>
 
 /**
  * Get a value for the specified option.
  */
-std::string_view getOption(const std::vector<std::string_view> &args,
-                           const std::string_view optName);
+std::string getArgValue(const std::vector<std::string_view> &args,
+                        const std::string_view optName);
 
 /**
  * Checks if an option exists
  */
-bool hasOption(const std::vector<std::string_view> &args,
-               const std::string_view &option_name);
+bool hasArg(const std::vector<std::string_view> &args,
+            const std::string_view &option_name);
--- sarlab-tdc/src/argparser.cpp
+++ sarlab-tdc/src/argparser.cpp
@@ -1,24 +1,51 @@
 /* Standard library headers */
+#include <algorithm>
+#include <cctype>
+#include <string>
 #include <string_view>
 #include <vector>
 
 /* Class header */
 #include "argparser.h"
 
-std::string_view getOption(const std::vector<std::string_view> &args,
-                           const std::string_view optName)
+std::string getArgValue(const std::vector<std::string_view> &args,
+                        const std::string_view optName)
 {
+    std::string optVal;
+    bool found = false;
+
     for (auto it = args.begin(), end = args.end(); it != end; ++it) {
-        if (*it == optName)
-            if (it + 1 != end)
-                return *(it + 1);
+        if (*it == optName) {
+            if (it + 1 != end) {
+                optVal = *(it + 1);
+                found = true;
+                break;
+            }
+        }
     }
 
-    return "";
+    if (!found) {
+        return "";
+    }
+
+    // Trim whitespace from the start of the option value
+    optVal.erase(optVal.begin(), std::find_if(optVal.begin(), optVal.end(),
+                                              [](unsigned char ch) {
+                                                  return !std::isspace(ch);
+                                              }));
+
+    // Trim whitespace from the end of the option value
+    optVal.erase(
+        std::find_if(optVal.rbegin(), optVal.rend(),
+                     [](unsigned char ch) { return !std::isspace(ch); })
+            .base(),
+        optVal.end());
+
+    return optVal;
 }
 
-bool hasOption(const std::vector<std::string_view> &args,
-               const std::string_view &optName)
+bool hasArg(const std::vector<std::string_view> &args,
+            const std::string_view &optName)
 {
     for (auto it = args.begin(), end = args.end(); it != end; ++it) {
         if (*it == optName)
