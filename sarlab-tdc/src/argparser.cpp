c3b339ce725407118ca190a58a32c5505a4f901d
Wyatt Gronnemose <wgronnemose@airadar.com>
2024-07-21 21:57:48 -0700

    Super simple argument parsing

--- sarlab-tdc/src/argparser.cpp
+++ sarlab-tdc/src/argparser.cpp
@@ -8,11 +8,22 @@
 std::string_view getOption(const std::vector<std::string_view> &args,
                            const std::string_view optName)
 {
+    for (auto it = args.begin(), end = args.end(); it != end; ++it) {
+        if (*it == optName)
+            if (it + 1 != end)
+                return *(it + 1);
+    }
+
     return "";
 }
 
 bool hasOption(const std::vector<std::string_view> &args,
-               const std::string_view &option_name)
+               const std::string_view &optName)
 {
+    for (auto it = args.begin(), end = args.end(); it != end; ++it) {
+        if (*it == optName)
+            return true;
+    }
+
     return false;
 }
