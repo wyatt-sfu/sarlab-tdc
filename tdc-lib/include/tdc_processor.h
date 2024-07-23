07581f01645d260fa12fd6546d2e8eb7c896626b
Wyatt Gronnemose <wgronnemose@airadar.com>
2024-07-22 17:06:34 -0700

    Started on the interface for the tdc processor

--- tdc-lib/include/tdc_processor.h
+++ tdc-lib/include/tdc_processor.h
@@ -1,2 +1,34 @@
+/* Standard library headers */
+#include <complex>
+
 class TdcProcessor
-{};
+{
+public:
+    TdcProcessor(int gpuNum);
+
+    /**
+     * Start the SAR processor. Returns when the processing is complete.
+     */
+    void start();
+
+    /**
+     * Configures the raw radar data input to the processor.
+     * 
+     * Arguments:
+     * -----------
+     * dataBuffer: Pointer to a 2D array of raw IQ data of size nPri x nSamples.
+     *             The data is stored row-wise.
+     * priTimes: Pointer to a 1D array containing the start time of each PRI.
+                 The size is nPri.
+     * nPri: Number of PRIs in raw data.
+     * nSamples: Number of samples per PRI.
+     * modRate: The modulation rate in Hz/s.
+     * sampleRate: The data sample rate in Hz.
+     */
+    void setRawData(const std::complex<float> *dataBuffer,
+                    const float *priTimes, int nPri, int nSamples,
+                    float modRate, float sampleRate);
+
+private:
+    int gpuNum = 0;
+};
--- tdc-lib/src/tdc_processor.cpp
+++ tdc-lib/src/tdc_processor.cpp
@@ -1,2 +1,18 @@
+/* Standard library headers */
+#include <complex>
+
 /* Class header */
 #include "tdc_processor.h"
+
+TdcProcessor::TdcProcessor(int gpuNum)
+{
+    this->gpuNum = gpuNum;
+}
+
+void TdcProcessor::start() {}
+
+void TdcProcessor::setRawData(const std::complex<float> *dataBuffer,
+                              const float *priTimes, int nPri, int nSamples,
+                              float modRate, float sampleRate)
+{
+}
