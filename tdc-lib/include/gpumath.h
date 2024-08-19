4e354b0a429e04488c957eccec300b182c2a69d6
Wyatt Gronnemose <wgronnemose@airadar.com>
2024-08-19 15:40:42 -0700

    Sum operator for float2 values

--- tdc-lib/include/gpumath.h
+++ tdc-lib/include/gpumath.h
@@ -7,6 +7,15 @@
 #include <vector_types.h>
 
 /**
+ * Sum operator for float2 arrays (which we use to represent complex numbers)
+ */
+__host__ __device__ inline float2 operator+(const float2 &lhs,
+                                            const float2 &rhs)
+{
+    return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
+
+/**
  * Dot product of 3d vectors
  */
 __host__ __device__ inline float v3_dot(float3 const *v1, float3 const *v2)
